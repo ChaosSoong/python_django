805bac19809dc92f470dd57f55e8664254ed5639
Chaos Soong <13313020563@163.com>
2016-08-02 11:41:50 +0800

    Update views.py

--- mysite/views.py
+++ mysite/views.py
@@ -12,6 +12,5 @@ def hours_ahead(request, offset):
     except ValueError:
         raise Http404()
     dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
-	assert False
     html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
     return HttpResponse(html)
