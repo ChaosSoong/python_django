6f6dc7335f37ed73d9d892652e6132834687bd0d
chaos <13313020563@163.com>
2016-08-03 14:40:45 +0800

    use {%block%}{endblock}

--- mysite/settings.py
+++ mysite/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(os.path.dirname(__file__), 'templates').replace('\\','/'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- mysite/views.py
+++ mysite/views.py
@@ -1,11 +1,10 @@
 from django.http import HttpResponse,Http404
+from django.template.loader import get_template
+from django.template import Context
 import datetime
 def hello(request):
     return HttpResponse("<em>Hello world</em>")
-def current_datetime(request):
-    now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
-    return HttpResponse(html)
+
 def hours_ahead(request, offset):
     try:
         offset = int(offset)
@@ -13,4 +12,10 @@ def hours_ahead(request, offset):
         raise Http404()
     dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
     html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
+    return HttpResponse(html)
+
+def current_datetime(request):
+    now = datetime.datetime.now()
+    t = get_template('current_datetime.html')
+    html = t.render(Context({'current_date': now}))
     return HttpResponse(html)
