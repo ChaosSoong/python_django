29a7f67c771abc4b04a0c9db9b5cc31d13fdc0a2
chaos <13313020563@163.com>
2016-08-05 10:58:13 +0800

    update admin end

--- books/admin.py
+++ books/admin.py
@@ -1,9 +1,19 @@
+# -*-coding:utf-8 -*-
 from django.contrib import admin
 
 # Register your models here.
 
 from books.models import Publisher, Author, Book
 
+class AuthorAdmin(admin.ModelAdmin):
+	list_display = ('first_name','last_name','email')
+	search_fields = ('first_name', 'last_name')
+class BookAdmin(admin.ModelAdmin):
+	list_display = ('title', 'publisher', 'publication_date')
+	list_filter = ('publication_date',)#过滤
+	date_hierarchy = 'publication_date'
+	ordering = ('-publication_date',)#排序
+	#fields = ('title', 'authors', 'publisher', 'publication_date')#自定义编辑表单
 admin.site.register(Publisher)
-admin.site.register(Author)
-admin.site.register(Book)
+admin.site.register(Author,AuthorAdmin)
+admin.site.register(Book,BookAdmin)
