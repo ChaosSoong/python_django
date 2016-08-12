c6d3b0cf18cc9b15262aa81413b750cacdca0c71
chaos <13313020563@163.com>
2016-08-12 17:34:01 +0800

    modified format

--- contact/forms.py
+++ contact/forms.py
@@ -4,9 +4,10 @@ class ContactForm(forms.Form):
     subject = forms.CharField()
     email = forms.EmailField(required=False)
     message = forms.CharField()
-	def clean_message(self):
-        message = self.cleaned_data['message']
-        num_words = len(message.split())
-        if num_words < 4:
-            raise forms.ValidationError("Not enough words!")
-        return message
+	
+def clean_message(self):
+    message = self.cleaned_data['message']
+    num_words = len(message.split())
+    if num_words < 4:
+        raise forms.ValidationError("Not enough words!")
+    return message
