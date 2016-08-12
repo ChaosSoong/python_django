42e4638bd187582d6182fc186d4408d44a36d3b9
chaos <13313020563@163.com>
2016-08-12 15:09:20 +0800

    update form

--- books/views.py
+++ books/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
+from books.models import Book
 
 # Create your views here.
 def search_form(request):
     return render_to_response('search_form.html')
 def search(request):
+    error = False
     if 'q' in request.GET:
-        message = 'You searched for: %r' % request.GET['q']
-    else:
-        message = 'You submitted an empty form.'
-    return HttpResponse(message)
+        q = request.GET['q']
+        if not q:
+            error = True
+        else:
+            books = Book.objects.filter(title__icontains=q)
+            return render_to_response('search_results.html',
+                {'books': books, 'query': q})
+    return render_to_response('search_form.html',
+        {'error': error})
