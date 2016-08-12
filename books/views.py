76f6c77db17f30b296a6e26fed80f7451ebe09e3
chaos <13313020563@163.com>
2016-08-12 15:42:15 +0800

    form validate

--- books/views.py
+++ books/views.py
@@ -6,14 +6,16 @@ from books.models import Book
 def search_form(request):
     return render_to_response('search_form.html')
 def search(request):
-    error = False
+    errors = []
     if 'q' in request.GET:
         q = request.GET['q']
         if not q:
-            error = True
+            errors.append('Enter a search term.')
+        elif len(q) > 20:
+            errors.append('Please enter at most 20 characters.')
         else:
             books = Book.objects.filter(title__icontains=q)
             return render_to_response('search_results.html',
                 {'books': books, 'query': q})
     return render_to_response('search_form.html',
-        {'error': error})
+        {'errors': errors })
